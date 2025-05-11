352d2c6c22a85daed4794395d668fcab1ec129c1
andyzDEV-alt <r.o.sea.nnd.ina.ya.ng@gmail.com>
2025-05-11 08:16:49 +0000

    ADDED NEW PING

--- py.py
+++ py.py
@@ -4,4 +4,5 @@ DARK README
 docker-compose up -d && docker-compose logs -f
 2.PlayIt.gg setup Just Port forward and set ip on playit gg 0.0.0.0 port use 
 the forwarded port
-COMMAND : playit
+COMMAND : playit or 
+docker run --rm -it --net=host -e SECRET_KEY=7a610abcd75c9f70fdb66a2b4bd01dacd312b248dcac6498ef731d5fdcbcd644 ghcr.io/playit-cloud/playit-agent:0.15
